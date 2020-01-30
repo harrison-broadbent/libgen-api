d6598a98b3b216dc01ea359d16af0dd75b4d37a6
Harrison Broadbent <harrisonbroadbent@gmail.com>
2020-01-30 21:32:31 +1100

    Fix issue where the title text will be replaced with the title links. Furthur testing required to determine whether other critical issues are present.

--- CLI-Pirate.py
+++ CLI-Pirate.py
@@ -8,6 +8,8 @@ col_names = ["ID", "Author", "Title", "Publisher", "Year", "Pages", "Language", 
             "Mirror_1", "Mirror_2", "Mirror_3", "Mirror_4", "Mirror_5", "Edit"]
 
 book_name = "the killing floor"
+# book_name = "eragon"
+
 book_name_parsed = "%20".join(book_name.split(" "))
 
 search_url = f'http://gen.lib.rus.ec/search.php?req={book_name_parsed}'
@@ -21,10 +23,15 @@ for subheading in subheadings:
 
 information_table = soup.find_all('table')[2]
 
+data1 = []
+for row in information_table.find_all('tr'):
+    col_array = []
+    for td in row.find_all("td"):
+        if td.find('a') and td.find('a').has_attr("title") and td.find('a')["title"] is not "":
+            col_array.append(td.a['href'])
+        else:
+            col_array.append(''.join(td.stripped_strings))
+    data1.append(col_array)
 
-data = [[td.a['href'] if td.find('a') and td.find('a').has_attr("title") else 
-            ''.join(td.stripped_strings)
-            for td in row.find_all('td')]
-        for row in information_table.find_all('tr')]
-link_df = pd.DataFrame(data[1:], columns=col_names)
-print(link_df["Title"])
+link_dfn = pd.DataFrame(data1[1:], columns=col_names)
+print(link_dfn["Title"])
