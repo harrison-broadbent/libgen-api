8922e4fa1cc3815f78f3c00ea9302664ff1d773d
Noah <noah@karat.io>
2021-03-27 20:26:06 +0000

    Added feature to filter results based upon partial and case-insensitive string matching, added option to perform full match, removed self references, removed dict references in function parameters

--- libgen_API/libgen_search.py
+++ libgen_API/libgen_search.py
@@ -1,62 +1,49 @@
 from .search_request import SearchRequest
-import requests
-from bs4 import BeautifulSoup
+
 
 class LibgenSearch:
-	
-	def search_title(self, query):
-		self.search_request = SearchRequest(query, search_type="title")
-		return self.search_request.aggregate_request_data()
+    def search_title(self, query):
+        search_request = SearchRequest(query, search_type="title")
+        return search_request.aggregate_request_data()
 
-	def search_author(self, query):
-		self.search_request = SearchRequest(query, search_type="author")
-		return self.search_request.aggregate_request_data()
+    def search_author(self, query):
+        search_request = SearchRequest(query, search_type="author")
+        return search_request.aggregate_request_data()
 
-	def search_title_filtered(self, query, filters = {	"ID": "",
-														"Author": "", 
-														"Title": "", 
-														"Publisher": "", 
-														"Year": "",
-														"Pages": "",
-														"Language": "",
-														"Size": "",
-														"Extension": "", 
-														"Mirror_1": "",
-														"Mirror_2": "",
-														"Mirror_3": "",
-														"Mirror_4": "",
-														"Mirror_5": "",
-														"Edit": ""	
-														}):
-		self.search_request = SearchRequest(query, search_type="title")
-		data = self.search_request.aggregate_request_data()
-		
-		filtered_data = data
-		for f in filters:
-			filtered_data = [d for d in filtered_data if d[f] in filters.values()]
-		return filtered_data
+    def search_title_filtered(self, query, filters, exact_match=False):
+        search_request = SearchRequest(query, search_type="title")
+        results = search_request.aggregate_request_data()
+        filtered_results = filter_results(results=results, filters=filters, exact_match=exact_match)
+        return filtered_results
 
-	def search_author_filtered(self, query, filters = {	"ID": "",
-														"Author": "", 
-														"Title": "", 
-														"Publisher": "", 
-														"Year": "",
-														"Pages": "",
-														"Language": "",
-														"Size": "",
-														"Extension": "", 
-														"Mirror_1": "",
-														"Mirror_2": "",
-														"Mirror_3": "",
-														"Mirror_4": "",
-														"Mirror_5": "",
-														"Edit": ""	
-														}):
-		self.search_request = SearchRequest(query, search_type="author")
-		data = self.search_request.aggregate_request_data()
-		
-		filtered_data = data
-		for f in filters:
-			filtered_data = [d for d in filtered_data if d[f] in filters.values()]
-		return filtered_data
-		
+    def search_author_filtered(self, query, filters, exact_match=False):
+        search_request = SearchRequest(query, search_type="author")
+        results = search_request.aggregate_request_data()
+        filtered_results = filter_results(results=results, filters=filters, exact_match=exact_match)
+        return filtered_results
+
+
+def filter_results(results, filters, exact_match):
+    """
+    Returns a list of results that satisfy the given filter criteria.
+    When exact_match is true, it is checked if the filters are a subset of the results dict.
+    When exact-match is false, a case-insensitive partial string match is performed iteratively.
+    """
+    filtered_list = []
+    if exact_match:
+        for result in results:
+            if filters.items() <= result.items():
+                filtered_list.append(result)
+
+    else:
+        filter_matches_result = False
+        for result in results:
+            for field, query in filters.items():
+                if query.casefold() in result[field].casefold():
+                    filter_matches_result = True
+                else:
+                    filter_matches_result = False
+                    break
+            if filter_matches_result:
+                filtered_list.append(result)
+    return filtered_list
